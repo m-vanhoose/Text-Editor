56ab616d338a25985c306b49974ad5ffd1913e84
m-vanhoose <m-vanhoose@live.com>
2023-06-12 21:56:10 -0400

    fixed functionality

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -26,8 +26,8 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      console.log(typeof data.value);
-      this.editor.setValue(data.value || localData || header);
+      // console.log(typeof data.value);
+      this.editor.setValue(data || localData || header);
     });
 
     this.editor.on('change', () => {
